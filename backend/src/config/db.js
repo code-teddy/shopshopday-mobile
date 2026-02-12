3cd33567a60f7a0d1c99d02d8fa281ffc3131ff4
Chan Tung Wa Teddy (陳東偉) <teddy.chan@candcprinting.com>
2026-02-12 12:42:26 +0800

    startServer method added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,7 @@ export const connectDB = async() => {
         const conn = await mongoose.connect(ENV.DB_URL)
         console.log(`Connected to MONGODB: ${conn.connection.host}`)
     } catch (error) {
-        console.error("MONGODB connection error")
+        console.error("MONGODB connection error:", error.message)
         process.exit(1) // exit code 1 means failure, 0 means success
     }
 
