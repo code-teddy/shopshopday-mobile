6a2199908cbcd3b91f47304c668ded3328f2bdb9
Chan Tung Wa Teddy (陳東偉) <teddy.chan@candcprinting.com>
2026-02-16 11:53:16 +0800

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,18 @@ import path from "path";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express"
+import { functions, inngest } from "./config/inngest.js"
+
 
 const app = express();
 
 const __dirname = path.resolve();
 
-
+app.use(express.json)
 app.use(clerkMiddleware()) // add auth object under the req => req.auth
+
+app.use("/api/inngest", serve({client:inngest, functions:functions}))
 
 app.get("/api/health", (req, res) =>{
     res.status(200).json({message:"Success"});
